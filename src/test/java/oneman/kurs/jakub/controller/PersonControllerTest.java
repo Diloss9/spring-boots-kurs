17cdb29abe897fd7b15cd226e95d4f0d52036b5a
Jakub Waclawowicz <jakub.waclawowicz@mischok.de>
2025-05-06 13:26:31 +0200

    Add unit test to validate person list structure
    - Ensure the list contains exactly three elements.
    - Verify that each element has the properties: first_name, last_name, description, and date_of_birth.
    - Check that the second element has the correct values for first_name, last_name, and date_of_birth.
    - Ensure that the date_of_birth property is not empty for any element.

--- src/test/java/oneman/kurs/jakub/controller/PersonControllerTest.java
+++ src/test/java/oneman/kurs/jakub/controller/PersonControllerTest.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,8 +20,19 @@ class PersonControllerTest {
 
 	@Test
 	void checkForPersons() throws Exception {
-		this.mockMvc.perform(get("/persons"))
-				.andExpect(status().isOk());
-	}
+		mockMvc.perform(get("/persons"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(3)))
 
+				.andExpect(jsonPath("$[*].first_name").exists())
+				.andExpect(jsonPath("$[*].last_name").exists())
+				.andExpect(jsonPath("$[*].description").exists())
+				.andExpect(jsonPath("$[*].date_of_birth").exists())
+
+				.andExpect(jsonPath("$[*].date_of_birth").isNotEmpty())
+
+				.andExpect(jsonPath("$[1].first_name").value("Max"))
+				.andExpect(jsonPath("$[1].last_name").value("Mustermann"))
+				.andExpect(jsonPath("$[1].date_of_birth").value("1997-06-23"));
+	}
 }
