4698f231e2e1b5414fd80a31f7fe67deb51ee1b2
Jakub Waclawowicz <jakub.waclawowicz@mischok.de>
2025-05-06 09:16:07 +0200

    packages to lowercase

--- src/test/java/oneman/kurs/jakub/controller/PersonControllerTest.java
+++ src/test/java/oneman/kurs/jakub/controller/PersonControllerTest.java
@@ -1,26 +1,26 @@
-package oneMan.Kurs.Jakub.controller;
+package oneman.kurs.jakub.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class TestForProject {
+class PersonControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
 
-	
+
 	@Test
 	void checkForPersons() throws Exception {
 		this.mockMvc.perform(get("/persons"))
-				.andExpect(status().isOk());
+				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
