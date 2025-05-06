57d5a60aba2069c9e5b69c37ed86aebde0e76df6
Jakub Waclawowicz <jakub.waclawowicz@mischok.de>
2025-05-06 09:46:03 +0200

    Clean up incorrect code from last commit

--- src/test/java/oneman/kurs/jakub/controller/PersonControllerTest.java
+++ src/test/java/oneman/kurs/jakub/controller/PersonControllerTest.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,11 +16,10 @@ class PersonControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-
 	@Test
 	void checkForPersons() throws Exception {
 		this.mockMvc.perform(get("/persons"))
-				.andExpect(MockMvcResultMatchers.status().isOk());
+				.andExpect(status().isOk());
 	}
 
 }
